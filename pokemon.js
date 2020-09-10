d3c8b2e5c82ac5d62a05f48a11e2334c57eb169c
Jesus Armando Zamora Gamez <armando.zamora14@hotmail.com>
2020-09-09 17:23:57 -0700

    Pokemon

--- pokemon.js
+++ pokemon.js
@@ -1,51 +1,76 @@
 const urlList = JSON.parse(sessionStorage.getItem('urlList'));
 const sprite = sessionStorage.getItem('sprite');
+const pokemon = {
+    sprite: document.getElementById('pokemon-image'),
+    name: document.getElementById('pokemon-name'),
+    types: document.getElementById('pokemon-types'),
+    abilities: document.getElementById('pokemon-abilities')
+}
 
-const pokemonName = document.getElementById('pokemon-name');
-const pokemonImage = document.getElementById('pokemon-image');
-const pokemonType = document.getElementById('pokemon-types');
-const abilitiesPokemon = document.getElementById('pokemon-abilities');
-
-const GetPokemon =()=>{
-    const spriteName = sprite.replace('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/','');
-    const spriteNumber = spriteName.replace ('.png','');
-    console.log(sprite);
-    
+//console.log(urlList);
+const GetPokemon = () => {
+    const spriteName = sprite.replace('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/', '');
+    const spriteNumber = spriteName.replace('.png', '');
+    console.log(spriteNumber);
     urlList.forEach(element => {
-        let pokemonNumber = element.replace('https://pokeapi.co/api/v2/pokemon/','');
-        pokemonNumber= pokemonNumber.replace('/', '');
-        if (pokemonNumber == spriteNumber)
-        {
-            showPokemon(element);
-            pokemonImage.innerHTML = `<img src="${sprite}"  alt="Pokemon">`;
-            
+        let pokemonNumber = element.replace('https://pokeapi.co/api/v2/pokemon/', '');
+        pokemonNumber = pokemonNumber.replace('/', '');
+        if (spriteNumber == pokemonNumber) {
+            console.log(element);
+            GetPokemonInfo(element);
+        }
+    });
+}
+
+const GetPokemonInfo = async (url) => {
+    const data = await fetch(url);
+    const dataJson = await data.json();
+    const { sprites, name, types, abilities } = dataJson;
+    types.forEach(element => { //forEach para recorrer arreglos
+          const {type} = element;
+    //      console.log(type.name);
+          pokemon.types.innerHTML += `<li>${type.name}</li>`;
+
+      });
+
+      abilities.forEach(element => {
+          const{ability}= element;
+    //      //pokemon.abilities.innerHTML += `<li>${ability.name}</li>`;
+    //      //console.log(ability.url);
+         GetAbilityInfo(ability.url, ability.name);
+     })
+    // console.log(await abilities);
+    pokemon.sprite.src = await dataJson.sprites['front_default'];
+    pokemon.name.innerHTML= await `Name: ${name}`;
+    //return await sprites.front_default;
+}
+
+const GetAbilityInfo= async (url, abilityName)=>{
+    const data= await fetch(url);
+    const dataJson= await data.json();
+    const{effect_entries}= dataJson;
+
+    let effectsList='';
+
+    effect_entries.forEach(element => {
+
+        const{effect, language} = element;
+
+        if(language.name === 'en'){
+            //console.log(effect);
+            effectsList += `<li>${effect}</li>`;
         }
     });
 
-}
-
-const showPokemon = async url =>{
-    fetch(url).then(data => data.json()).then(json=>{
-        console.log(json.types);        //Conseguimos el nombre del pokemon
-        const pokemonShowName= json.name;
-        console.log(pokemonShowName);
-        pokemonName.innerHTML = pokemonShowName;
-
-       const pokemonTypes =  json.types;
-       pokemonTypes.forEach(type=>{         //Obtenemos los types del pokemon
-           const typeName= type.type.name;
-           pokemonType.innerHTML += `<li> ${typeName} </li>`;
-           console.log(typeName);
-       })
-       
-       const pokemonAbilities = json.abilities;
-       pokemonAbilities.forEach(ability =>{         //Mostramos las habilidades del pokemon
-        const abilityName = ability.ability.name;
-        abilitiesPokemon.innerHTML += `<li> ${abilityName} </li>`;
-        console.log(abilityName);
-       })
-    })
-
+    pokemon.abilities.innerHTML += 
+    `<li>
+        ${abilityName}
+        <div>effect</div>
+        <ul>
+            ${effectsList}
+        </ul>
+    </li> `;
+    //console.log(await dataJson);
 }
 
 GetPokemon();
